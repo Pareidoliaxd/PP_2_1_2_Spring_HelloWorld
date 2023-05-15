eac2e3cda3f34c1d71a98b993d1e694942f19fb9
Pareidoliaxd <gold.apple5@mail.ru>
2023-05-15 16:07:00 +0300

    1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat catBean = applicationContext.getBean(Cat.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+
+        System.out.println(catBean == catBean2);
     }
 }
